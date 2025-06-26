6725bf51de565dc9e457363eb8b4c8b4799efd35
muky <muky72000@gmail.com>
2025-06-26 18:02:45 +0200

    Add initial HTML structure and navigation
    - Created index.php with basic HTML structure and linked CSS and fonts.
    - Added nav.php for navigation bar with logo and action links.
    - Initialized script.js file for future JavaScript functionality.

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="app.css">
+    <link href="https://fonts.cdnfonts.com/css/glacial-indifference-2" rel="stylesheet">
+    <link href="https://fonts.cdnfonts.com/css/pompiere" rel="stylesheet">
+
+
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    include("nav.php");
+    ?>
+</body>
+
+</html>
---
+++ nav.php
@@ -0,0 +1,11 @@
+<nav>
+
+    <img class="logo" src="assets/logo.png" alt="">
+    <div class="actions">
+        <a href="">Consultations</a>
+        <a href="">Tarifs</a>
+        <a href="">Balance Inbody 120</a>
+    </div>
+
+
+</nav>
