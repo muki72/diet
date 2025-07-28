9ec2b974bf70f8440dbc700ee1d731ad17b07935
muky <muky72000@gmail.com>
2025-07-28 16:21:23 +0200

    Add content sections for introduction, reviews, reservations, and contact information

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="app.css">
     <link href="https://fonts.cdnfonts.com/css/glacial-indifference-2" rel="stylesheet">
     <link href="https://fonts.cdnfonts.com/css/pompiere" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.0/css/all.min.css" integrity="sha512-DxV+EoADOkOygM4IR9yXP8Sb2qwgidEmeqAEmDKIOfPRQZOWbXCzLC6vjbZyy0vPisbH2SyW27+ddLVCN+OMzQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 
 
@@ -31,8 +32,44 @@
                 <button>Reserver sur Doctolib</button>
             </div>
         </div>
+        <div class="intro">
+            <h2>Qui suis-je ?</h2>
 
+            <p>Passionnée par l’accompagnement humain, je suis titulaire d’un BTS ESF depuis 2016, ce qui m’a permis d’acquérir une solide expérience dans le domaine social, auprès de publics variés.
 
+                Intéressée depuis toujours par la nutrition, j’ai complété mon parcours en 2022 avec un BTS Diététique, pour associer mes compétences sociales à celles de l’alimentation.</p>
+
+            <p>Je me suis également formée à l’alimentation en pleine conscience, une approche qui aide à se reconnecter à ses sensations, mieux comprendre ses besoins, et retrouver une relation apaisée avec la nourriture.
+
+                Aujourd’hui, je vous reçois au cabinet ou me déplace à domicile avec ma balance professionnelle. En individuel ou en duo (couple, parent/enfant...), je propose un accompagnement personnalisé et adapté à vos besoins.</p>
+        </div>
+        <div class="review">
+            <h2>Les avis Google</h2>
+        </div>
+        <div class="reservation">
+            <h2>Réservation</h2>
+            <div class="btn-actions">
+                <button>Individuel</button>
+                <button>Couple</button>
+                <button>Bilan corporel</button>
+            </div>
+        </div>
+        <div class="place">
+            <h2>Le cabinet</h2>
+        </div>
+        <div class="contact">
+            <h2>Contact</h2>
+            <div class="inline infos">
+                <div class="inline">
+                    <i class="fa-solid fa-phone"></i>
+                    <p>06 45 81 03 91</p>
+                </div>
+                <div class="inline">
+                    <i class="fa-solid fa-envelope"></i>
+                    <p>contact@lisamercierdieteticienne.fr</p>
+                </div>
+            </div>
+        </div>
 
     </div>
 </body>
